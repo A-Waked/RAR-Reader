fd73c2a2ae020e27c78a63847b0911852d9d681e
A-Waked <abdulw.3000@gmail.com>
2021-01-12 16:06:27 -0500

    add

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import sys
 
+def add():
+    return 1+1
+
 def main() -> None:
     print(f"Arguments count: {len(sys.argv)}")
-    
+
     for i, arg in enumerate(sys.argv):
         print(f"Argument {i:>6}: {arg}")
-
-    
 
 if __name__ == "__main__":
     main()
