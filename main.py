baf985077e7b09b30606062455224236ee532e2b
A-Waked <abdulw.3000@gmail.com>
2021-01-12 17:45:06 -0500

    read rar filetypes (.rar and .cbr)

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 import sys
+import rarfile
 
-def add():
-    return 1+1
+def readfile(fp):
+    rf = rarfile.RarFile(fp)
+    for f in rf.infolist():
+        print(f.filename, f.file_size)
+        if f.filename == "README":
+            print(rf.read(f))
 
 def main() -> None:
     print(f"Arguments count: {len(sys.argv)}")
 
     for i, arg in enumerate(sys.argv):
         print(f"Argument {i:>6}: {arg}")
+
+    fp = sys.argv[1]
+    print(fp)
+
+    readfile(fp)
 
 if __name__ == "__main__":
     main()
