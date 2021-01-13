2d587bc4e0b55d82b12b8e1b797be9b5c6ada998
A-Waked <abdulw.3000@gmail.com>
2021-01-13 15:58:37 -0500

    improved args

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import sys
+import getopt
+import argparse
+import os
+from os import listdir
+from os.path import isfile, join
 import rarfile
+import zipfile
+
+def getfiles(path) -> str:
+    print(path)
+    return [f for f in listdir(path) if isfile(join(path, f))]
 
 def readfile(fp):
     rf = rarfile.RarFile(fp)
@@ -9,15 +19,23 @@ def readfile(fp):
             print(rf.read(f))
 
 def main() -> None:
-    print(f"Arguments count: {len(sys.argv)}")
+    argv = sys.argv[1:]
 
-    for i, arg in enumerate(sys.argv):
-        print(f"Argument {i:>6}: {arg}")
+    ap = argparse.ArgumentParser(description='reads zip/rar type files and displays images found')
 
-    fp = sys.argv[1]
-    print(fp)
+    ap.add_argument('path', metavar='FP', type=str, nargs='*', default=[os.getcwd()],
+        help='file paths to read')
+    ap.add_argument('-a', action='store_true', default=False,
+        help='read all files in specified directory if specified or current directory if not provided')
 
-    readfile(fp)
+    args = ap.parse_args()
 
+    if isfile(args.path[0]) == False:
+        args.a = True
+
+    print(getfiles(args.path[0]))
+
+    print(args)
+    
 if __name__ == "__main__":
     main()
